3813a5826006149dda3bbc7520e859ba59d2305b
Ash Krishan <AKrishan@leighday.root>
2016-02-15 17:39:46 +0000

    Experimental Auth passport

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var expressSession = require('express-session');
 
 //DB
 mongoose.connect('mongodb://localhost/angdataapp');
@@ -12,12 +15,45 @@ var seedDb = require(__dirname + '/seedDb');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 //Middleware and initialising
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
 
+//User authentication middleware and setup
+app.use(session({
+    secret: '6g!zu%md&b=yxeef7v=5$(8mwyc*gkb_qhb83zm1slr#!yoli(',  //generated via django key generator
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //seedDb();
+
+
+//Function to chekc if user is autheticated and add that as middleware for requests and response
+
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated) {
+        res.next();
+    } else {
+        res.redirect('/login.html')
+    }
+    
+}
+
+
+app.get('/login', function(req, res) {
+    res.send() 
+     
+});
 
 app.get('/', function(req,res) {
     res.send(__dirname + '/public/index.html');
--- models/user.js
+++ models/user.js
@@ -1,10 +1,27 @@
+//var mongoose = require('mongoose');
+//
+//var userSchema = new mongoose.Schema({
+//	firstName: String,
+//	lastname: String,
+//	jobTitle: String		
+//});
+//
+//var User = mongoose.model('User', userSchema);
+//
+//module.exports = User;
+
 var mongoose = require('mongoose');
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = new mongoose.Schema({
-	firstName: String,
-	lastname: String,
-	jobTitle: String		
+    username: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    jobtitle: String,
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model('User', userSchema);
 
